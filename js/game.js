007945510d59ba8091ea6157469103d51a4df12f
zhaolinxu <zhaolinxu@sina.cn>
2021-11-10 13:26:31 +0800

    更新

--- js/game.js
+++ js/game.js
@@ -451,7 +451,7 @@ function gameLoop(diff) {
 }
 
 function hardReset(resetOptions) {
-	if (!confirm("Are you sure you want to do this? You will lose all your progress!")) return
+	if (!confirm("你确定要这么做吗？ 你将失去所有的进度！")) return
 	player = null
 	if(resetOptions) options = null
 	save(true);
